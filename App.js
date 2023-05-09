479099395cc7d9157939e942cf05d78b64eeb013
Skogab <114601288+Skogab@users.noreply.github.com>
2023-05-09 13:43:15 +0200

    5.3 firebase

--- App.js
+++ App.js
@@ -3,6 +3,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBH0QOL_ICNQq0vPy8miFmhxTg4BdY8ksM",
+	authDomain: "chatapp-41ca4.firebaseapp.com",
+	projectId: "chatapp-41ca4",
+	storageBucket: "chatapp-41ca4.appspot.com",
+	messagingSenderId: "789129214199",
+	appId: "1:789129214199:web:c90df4278ce1ab38c27bf2",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +26,7 @@ const App = () => {
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName="Start">
 				<Stack.Screen name="Start" component={Start} />
-				<Stack.Screen name="Chat" component={Chat} />
+				<Stack.Screen name="Chat">{(props) => <Chat {...props} db={db} />}</Stack.Screen>
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
--- components/Chat.js
+++ components/Chat.js
@@ -1,35 +1,34 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { collection, query, orderBy, onSnapshot, addDoc } from "firebase/firestore";
+// import { db } from "../firebase";
 
-const Chat = ({ route, navigation }) => {
+const Chat = ({ route, navigation, db }) => {
 	const [messages, setMessages] = useState([]);
 	const { name } = route.params;
 
 	useEffect(() => {
 		navigation.setOptions({ title: name });
-		setMessages([
-			{
-				_id: 1,
-				text: "Hello developer",
-				createdAt: new Date(),
-				user: {
-					_id: 2,
-					name: "React Native",
-					avatar: "https://placeimg.com/140/140/any",
-				},
-			},
-			{
-				_id: 2,
-				text: "This is a system message",
-				createdAt: new Date(),
-				system: true,
-			},
-		]);
+		const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+		const unsubMessages = onSnapshot(q, (docs) => {
+			let newMessages = [];
+			docs.forEach((doc) => {
+				newMessages.push({
+					id: doc.id,
+					...doc.data(),
+					createdAt: new Date(doc.data().createdAt.toMillis()),
+				});
+			});
+			setMessages(newMessages);
+		});
+		return () => {
+			if (unsubMessages) unsubMessages();
+		};
 	}, []);
 
 	const onSend = (newMessages) => {
-		setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
+		addDoc(collection(db, "messages"), newMessages[0]);
 	};
 
 	const renderBubble = (props) => {
