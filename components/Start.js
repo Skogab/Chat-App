312c862d1c4c97529e8acb965ec8805bb33c27d8
Skogab <114601288+Skogab@users.noreply.github.com>
2023-05-09 13:45:27 +0200

    Update Start.js

--- components/Start.js
+++ components/Start.js
@@ -1,9 +1,26 @@
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, ImageBackground } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, ImageBackground, Alert } from "react-native";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const Start = ({ navigation }) => {
 	const [text, setText] = useState("");
 	const [color, setColor] = useState("");
+
+	const auth = getAuth();
+
+	const signInUser = () => {
+		signInAnonymously(auth)
+			.then((result) => {
+				navigation.navigate("Chat", {
+					id: result.user.uid,
+					name: text ? text : "User",
+					color: color ? color : "white",
+				});
+			})
+			.catch((error) => {
+				Alert.alert("Unable to sign in, try again later.");
+			});
+	};
 
 	return (
 		<ImageBackground
@@ -15,7 +32,7 @@ const Start = ({ navigation }) => {
 					<Text style={styles.title}>Chat App!</Text>
 				</View>
 				<View style={styles.subContainer}>
-					<TextInput placeholder="Your name" style={styles.input} onChangeText={setText} />
+					<TextInput placeholder="Enter your name" style={styles.input} onChangeText={setText} />
 					<Text>Choose Background Color</Text>
 					<View style={styles.radioButtonContainer}>
 						<TouchableOpacity
@@ -31,15 +48,8 @@ const Start = ({ navigation }) => {
 							style={[styles.radioButton, { backgroundColor: "#fff869" }]}
 							onPress={() => setColor("#fff869")}></TouchableOpacity>
 					</View>
-					<TouchableOpacity
-						style={styles.button}
-						onPress={() =>
-							navigation.navigate("Chat", {
-								name: text ? text : "User",
-								color: color ? color : "white",
-							})
-						}>
-						<Text>Go to Chat</Text>
+					<TouchableOpacity style={styles.button} onPress={signInUser}>
+						<Text>Start Chatting</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
@@ -70,28 +80,30 @@ const styles = StyleSheet.create({
 	},
 	title: {
 		fontWeight: "bold",
-		fontSize: 30,
-		color: "white",
+		fontSize: 50,
+		color: "#FFFFFF",
+		marginBottom: 30,
+	},
+	input: {
+		borderColor: "#8A8F9E",
+		borderWidth: 1,
+		borderRadius: 3,
+		padding: 10,
+		width: "100%",
+		marginBottom: 20,
+	},
+	radioButton: {
+		height: 24,
+		width: 24,
+		borderRadius: 12,
 	},
 	button: {
 		alignItems: "center",
-		backgroundColor: "#DDDDDD",
-		padding: 10,
-	},
-	radioButton: {
-		backgroundColor: "black",
-		width: 30,
-		height: 30,
-		borderRadius: 15,
-	},
-	input: {
-		height: 40,
-		width: "88%",
-		margin: 12,
-		borderWidth: 3,
-		borderColor: "white",
-		padding: 10,
-		color: "white",
+		justifyContent: "center",
+		backgroundColor: "#757083",
+		padding: 20,
+		borderRadius: 8,
+		width: "100%",
 	},
 });
 
